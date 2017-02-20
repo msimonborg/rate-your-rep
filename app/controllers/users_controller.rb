1e66afa833a8cdd409748e8efacc7e8632a7e88e
M. Simon Borg <msimonborg@gmail.com>
2017-02-20 01:10:55 -0500

    add log in and sign up controller actions and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,43 @@
+class UsersController < ApplicationController
+  get '/login' do
+    if logged_in?
+      redirect "/users/#{current_user.id}"
+    else
+      erb :'users/login'
+    end
+  end
 
+  post '/login' do
+    @user = User.find_by email: params[:email]
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    else
+      flash[:message] = ["Incorrect email/password match, please try again."]
+      redirect '/login'
+    end
+  end
+
+  post '/signup' do
+    @user = User.new params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    else
+      flash[:message] = ["Incorrect email/password match, please try again."]
+      redirect '/signup'
+    end
+  end
+
+  get '/signup' do
+    if logged_in?
+      redirect "/users/#{current_user.id}"
+    else
+      erb :'users/signup'
+    end
+  end
+
+  get '/users/:id' do
+    erb :'users/show'
+  end
+end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,76 @@ require 'spec_helper'
 
 describe UsersController do
   let(:helpers) { TestHelper.new }
+
+  describe 'Log In' do
+    before do
+      @user1 = User.create(name: 'Test User', email: 'email@example.com', password: 'password')
+    end
+
+    it 'logs in with valid email and password' do
+      params = { email: 'email@example.com', password: 'password' }
+      post '/login', params
+      expect(last_response.status).to eq(302)
+      expect(last_response.location).to include("/users/#{@user1.id}")
+    end
+
+    it 'does not log in a user if user\'s email is not found' do
+      params = { email: 'wrong_email@bad.com', password: 'password' }
+      post '/login', params
+      expect(last_response.status).to eq(302)
+      expect(last_response.location).to include('/login')
+    end
+
+    it 'does not log in a user if the password doesn\'t match' do
+      params = { email: 'email@example.com', password: 'password1' }
+      post '/login', params
+      expect(last_response.status).to eq(302)
+      expect(last_response.location).to include('/login')
+    end
+  end
+
+  describe 'Sign Up' do
+    it 'succeeds with valid name, email, and password' do
+      params = { name: 'Test User', email: 'email@example.com', password: 'password' }
+      post '/signup', params
+      user = User.last
+      expect(last_response.status).to eq(302)
+      expect(last_response.location).to include("/users/#{user.id}")
+    end
+
+    it 'fails with blank name' do
+      params = { name: '', email: 'email@example.com', password: 'password' }
+      post '/signup', params
+      expect(last_response.status).to eq(302)
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'fails with invalid email' do
+      params = { name: 'Test User', email: 'email@', password: 'password' }
+      post '/signup', params
+      expect(last_response.status).to eq(302)
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'fails with blank email' do
+      params = { name: 'Test User', email: '', password: 'password' }
+      post '/signup', params
+      expect(last_response.status).to eq(302)
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'fails with invalid password' do
+      params = { name: 'Test User', email: 'email@example.com', password: 'passwor' }
+      post '/signup', params
+      expect(last_response.status).to eq(302)
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'fails with blank email' do
+      params = { name: 'Test User', email: 'email@example.com', password: '' }
+      post '/signup', params
+      expect(last_response.status).to eq(302)
+      expect(last_response.location).to include('/signup')
+    end
+  end
 end
