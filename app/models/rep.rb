a5e6e850ad6cc941ce6474d2d23113a9f147132e
M. Simon Borg <msimonborg@gmail.com>
2017-02-20 14:54:24 -0500

    clean up style in rep model

--- app/models/rep.rb
+++ app/models/rep.rb
@@ -2,19 +2,19 @@ class Rep < ActiveRecord::Base
   extend APIFindable::ClassMethods
   include APIFindable::InstanceMethods
 
-  has_many   :office_locations,
-             dependent: :destroy,
-             foreign_key: :bioguide_id,
-             primary_key: :bioguide_id
   has_many :calls, foreign_key: :bioguide_id, primary_key: :bioguide_id
   has_many :users, through: :calls
+  has_many :office_locations, dependent: :destroy,
+                              foreign_key: :bioguide_id,
+                              primary_key: :bioguide_id
 
   validates :bioguide_id, presence: true, uniqueness: true
-  validates :official_full, presence: true
-  validates :given_name, presence: true
-  validates :family_name, presence: true
-  validates :honorific_prefix, presence: true
-  validates :party_identification, presence: true
+  validates :official_full,
+            :given_name,
+            :family_name,
+            :honorific_prefix,
+            :party_identification,
+            presence: true
 
   attr_accessor :url, :photo, :twitter, :facebook, :youtube, :instagram, :state, :district
 end
