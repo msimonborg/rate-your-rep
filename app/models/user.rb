89b8b2486ad7312b3a74cdf1c665606062663544
M. Simon Borg <msimonborg@gmail.com>
2017-02-20 19:43:46 -0500

    add slug to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,15 @@ class User < ActiveRecord::Base
   has_many :office_locations, through: :calls
 
   has_secure_password
-  validates :username, presence: true, uniqueness: true, length: { minimum: 0 }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 8 }
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
                               on: :create
+  after_create :add_slug
+
+  def add_slug
+    slug = username.split(' ').join('-').gsub(/(\.|!|\?|\(|\)|&|%|@)/, '').gsub('$', 's')
+    update slug: slug.downcase
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220191346) do
+ActiveRecord::Schema.define(version: 20170221004104) do
 
   create_table "calls", force: :cascade do |t|
     t.string   "bioguide_id"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170220191346) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "slug"
   end
 
 end
