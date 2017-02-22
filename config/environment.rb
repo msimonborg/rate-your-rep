a7754c1fd038c0495edcf430d22f95dbff9bc1cf
M. Simon Borg <msimonborg@gmail.com>
2017-02-22 17:05:41 -0500

    a little more prod db config

--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,13 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-if ENV['SINATRA_ENV'] = 'development'
+if ENV['SINATRA_ENV'] == 'development' || ENV['SINATRA_ENV'] == 'test'
   ActiveRecord::Base.establish_connection(
     :adapter => "sqlite3",
     :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
   )
+elsif ENV['SINATRA_ENV'] == 'production'
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 end
 
 require_all 'app'
