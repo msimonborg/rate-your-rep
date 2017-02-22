04fd4934f0e3a5654fef26d5cfea25106aaa0608
M. Simon Borg <msimonborg@gmail.com>
2017-02-22 17:18:03 -0500

    tweak the config

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,12 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-if ENV['SINATRA_ENV'] == 'development' || ENV['SINATRA_ENV'] == 'test'
+if development?
   ActiveRecord::Base.establish_connection(
     :adapter => "sqlite3",
     :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
   )
-elsif ENV['SINATRA_ENV'] == 'production'
+elsif production?
   ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 end
 
