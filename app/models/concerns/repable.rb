f46a578570916051cd855bbb9c12d94fa5b0091b
M. Simon Borg <msimonborg@gmail.com>
2017-02-27 22:32:38 -0500

    rubocop repable.rb

--- app/models/concerns/repable.rb
+++ app/models/concerns/repable.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Shared methods for objects that belong_to a rep
 module Repable
   def rep_party
     rep.party_identification
@@ -11,6 +14,9 @@ module Repable
     rep.official_full
   end
 
+  # Objects that belong_to a :rep have the same :bioguide_id as their parent.
+  # It is the foreign key on the table. :rep_bioguide_id acts as an alias
+  # for :bioguide_id to make the connection more obvious in the views.
   def rep_bioguide_id
     bioguide_id
   end
