e947950dba88f25a23ffc5e6ecd4d7f0b99f5d34
M. Simon Borg <msimonborg@gmail.com>
2017-02-27 21:09:58 -0500

    rubocop call.rb

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# Join table between users, reps, and office_locations.
+# Attributes are the call stats and reviews for other models.
 class Call < ActiveRecord::Base
   include Repable
 
@@ -6,18 +10,22 @@ class Call < ActiveRecord::Base
   belongs_to :rep, foreign_key: :bioguide_id, primary_key: :bioguide_id
 
   validates :bioguide_id, :rating, presence: true
-  validates :rating, inclusion: { in: (1..5).to_a,
-    message: "%{value} is not a valid rating" }
+  validates :rating, inclusion: {
+    in: (1..5).to_a,
+    message: '%{value} is not a valid rating'
+  }
 
-  scope :this_week, ->{ where(created_at: (Time.now.midnight - 7.day)..Time.now) }
-  scope :time_sorted, ->{ order('created_at DESC') }
-  scope :last_call, ->{ time_sorted.limit(1).first }
-  scope :stars, ->{ average('rating').round.to_i }
-  scope :average_rating, ->{ average('rating').round(1).to_f }
-  scope :that_connected, ->{ where(got_through: true) }
-  scope :that_were_busy, ->{ where(busy: true) }
-  scope :that_went_to_voice_mail, ->{ where(voice_mail: true) }
-  scope :that_hit_a_full_mailbox, ->{ where(mailbox_full: true) }
+  scope :this_week, -> {
+    where(created_at: (Time.now.midnight - 7.day)..Time.now)
+  }
+  scope :time_sorted, -> { order('created_at DESC') }
+  scope :last_call, -> { time_sorted.limit(1).first }
+  scope :stars, -> { average('rating').round.to_i }
+  scope :average_rating, -> { average('rating').round(1).to_f }
+  scope :that_connected, -> { where(got_through: true) }
+  scope :that_were_busy, -> { where(busy: true) }
+  scope :that_went_to_voice_mail, -> { where(voice_mail: true) }
+  scope :that_hit_a_full_mailbox, -> { where(mailbox_full: true) }
 
   def user_update(params)
     # When a user updates, reset boolean attributes to false except those
@@ -29,7 +37,7 @@ class Call < ActiveRecord::Base
   end
 
   def time
-    created_at.localtime.strftime("%-m/%-d/%y at %l:%M%P")
+    created_at.localtime.strftime('%-m/%-d/%y at %l:%M%P')
   end
 
   def phone_number
