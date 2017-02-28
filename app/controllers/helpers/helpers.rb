29109acab995b0020b12e9f708e6d436643308af
M. Simon Borg <msimonborg@gmail.com>
2017-02-27 22:46:00 -0500

    rubocop helper.rb

--- app/controllers/helpers/helpers.rb
+++ app/controllers/helpers/helpers.rb
@@ -1,18 +1,21 @@
+# frozen_string_literal: true
+
+# Helper methods for controllers and views
 module Helpers
   def logged_in?
-    !!session[:user_id]
+    !session[:user_id].blank?
   end
 
   def current_user
     logged_in? && User.find(session[:user_id])
   end
 
-  def partial(page, options={})
+  def partial(page, options = {})
     erb(page, options.merge!(layout: false))
   end
 
   def pluralize(count, singular, plural, inner_string: nil)
-    inner_string = inner_string + ' ' if inner_string
+    inner_string += ' ' if inner_string
     "#{count} #{inner_string}#{count == 1 ? singular : plural}"
   end
 
