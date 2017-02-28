bc2c57c2a2c74aab0ee89e49f94a8d318375c801
M. Simon Borg <msimonborg@gmail.com>
2017-02-27 22:22:54 -0500

    rubocop office_location.rb

--- app/models/office_location.rb
+++ app/models/office_location.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+# The OfficeLocation has the phone data and is the object on which
+# a call is made.
 class OfficeLocation < ActiveRecord::Base
   include Callable
   include Repable
@@ -6,15 +9,25 @@ class OfficeLocation < ActiveRecord::Base
   has_many :calls
   has_many :users, through: :calls
 
-  validates :bioguide_id, :phone, :locality, :region, :postal_code, :office_type, presence: true
+  validates :bioguide_id,
+            :phone,
+            :locality,
+            :region,
+            :postal_code,
+            :office_type,
+            presence: true
 
-  scope :most_called, ->{ select("*, count(calls.id) AS calls_count").
-    joins(:calls).
-    group("office_locations.id").
-    order("calls_count DESC") }
+  scope :most_called, -> {
+    select('*, count(calls.id) AS calls_count').
+      joins(:calls).
+      group('office_locations.id').
+      order('calls_count DESC')
+  }
 
-  scope :best_rated, ->{ select("*, avg(calls.rating) AS calls_rating").
-    joins(:calls).
-    group("office_locations.id").
-    order("calls_rating DESC") }
+  scope :best_rated, -> {
+    select('*, avg(calls.rating) AS calls_rating').
+      joins(:calls).
+      group('office_locations.id').
+      order('calls_rating DESC')
+  }
 end
