93cc9bf80b32d96523440701122192d26cbac41c
M. Simon Borg <msimonborg@gmail.com>
2017-03-28 18:50:06 -0400

    only return active reps

--- app/controllers/reps_controller.rb
+++ app/controllers/reps_controller.rb
@@ -3,7 +3,7 @@
 # Routing and controller actions for Rep views
 class RepsController < ApplicationController
   get '/reps' do
-    @reps = Rep.order('family_name ASC')
+    @reps = Rep.active.order('family_name ASC')
     erb :'reps/index'
   end
 
@@ -16,17 +16,17 @@ class RepsController < ApplicationController
   end
 
   get '/reps/select' do
-    @reps = Rep.order('family_name ASC')
+    @reps = Rep.active.order('family_name ASC')
     erb :'reps/select'
   end
 
   get '/reps/most-called' do
-    @reps = Rep.most_called
+    @reps = Rep.active.most_called
     erb :'reps/most_called'
   end
 
   get '/reps/best-rated' do
-    @reps = Rep.best_rated
+    @reps = Rep.active.best_rated
     erb :'reps/best_rated'
   end
 
