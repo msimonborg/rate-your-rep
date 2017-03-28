77deb8ad2fb6440fa8a39b618f679081fd4ac92a
M. Simon Borg <msimonborg@gmail.com>
2017-03-28 17:59:30 -0400

    don't expose user_id in view, find from sessions hash in controller

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -14,14 +14,14 @@ class CallsController < ApplicationController
 
   post '/calls' do
     if logged_in?
-      @call = Call.new params
-      if @call.save
+      call = current_user.calls.build(params)
+      if call.save
         flash[:message] = [
-          "Your call to #{@call.rep_name} was saved successfully!"
+          "Your call to #{call.rep_name} was saved successfully!"
         ]
         redirect '/calls/recent'
       else
-        flash[:message] = @call.errors.full_messages
+        flash[:message] = call.errors.full_messages
         redirect "/calls/new/#{params[:office_id]}"
       end
     else
@@ -35,8 +35,12 @@ class CallsController < ApplicationController
     # Don't allow the patch unless the user is logged_in and is the current_user
     redirect '/login' unless current_user == call.user
     if call.user_update params[:call]
-      call_anchor = "#{params[:path]}#call#{call.id}-card"
-      call_link = "<a href='#{call_anchor}'>here</a>"
+      # call_anchor = "#{params[:path]}#call#{call.id}-card"
+      call_link = "
+        <a href='#{params[:path]}#call#{call.id}-card'>
+          here
+        </a>
+      "
       flash[:message] = [
         "Your review was successfully updated. View it #{call_link}."
       ]
